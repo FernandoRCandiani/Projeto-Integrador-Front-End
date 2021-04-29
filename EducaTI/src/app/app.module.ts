00c7c2a8b18eef71903f67879bb28febbe239799
unknown <candianirfernando@gmail.com>
2021-04-29 15:51:51 -0300

    Componete Conta

--- EducaTI/src/app/app.module.ts
+++ EducaTI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContaComponent } from './conta/conta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContaComponent
   ],
   imports: [
     BrowserModule,
