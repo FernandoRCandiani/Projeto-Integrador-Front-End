b5ea4ab8dbee6c03ed81a494d453d64979ab7ee4
raquel.glopes <glopes.raquel@gmail.com>
2021-05-11 14:06:11 -0300

    criação do editar e delete

--- EducaTI/src/app/delete/delete-tema/delete-tema.component.ts
+++ EducaTI/src/app/delete/delete-tema/delete-tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Temas } from 'src/app/model/Temas';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-delete-tema',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-tema.component.css']
 })
 export class DeleteTemaComponent implements OnInit {
+  tema : Temas = new Temas()
+  constructor(
+    private temaService : TemaService,
+    private router : Router,
+    private route : ActivatedRoute
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    window.scroll(0,0)
 
-  ngOnInit(): void {
+    if (environment.token ==''){
+      this.router.navigate(['/entrar'])
+    }
+    let id = this.route.snapshot.params['id']
+    this.findById(id)
   }
-
+  findById(id:number){
+    this.temaService.getById(id).subscribe((resp:Temas)=>{
+      this.tema = resp
+    })
+  }
+  deleteTema(){
+    this.temaService.deleteTema(this.tema.id).subscribe(()=>{
+      alert('O tema foi deletado com sucesso.')
+      this.router.navigate(['/temas'])
+    })
+  }
 }
--- EducaTI/src/app/service/tema.service.ts
+++ EducaTI/src/app/service/tema.service.ts
@@ -41,6 +41,15 @@ export class TemaService {
     return this.http.post<Temas>('http://localhost:8080/tema', tema, this.token)
   }
 
+  putTema(tema:Temas) : Observable<Temas>{
+    return this.http.put<Temas>('http://localhost:8080/tema', tema,
+    this.token)
+  }
+
+  deleteTema(id : number) {
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
   
   
 
