7b33f98ec4943b2d3b599ab279eaf92a1e803c76
IgorPV <igor.pereira.vieira@gmail.com>
2021-05-14 00:26:08 -0300

    alteração menu, rodapé, página sobre a equipe

--- EducaTI/src/app/sobre-nos/sobre-nos.component.ts
+++ EducaTI/src/app/sobre-nos/sobre-nos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-sobre-nos',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreNosComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    window.scroll(0,0)
   }
 }
